62c27134c0ce1964647f40f131b6b7c1becec0b8
Matheus Fongaro <matheus.fongaro@gmail.com>
2025-10-08 10:55:43 -0400

    feat: create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+export function Header() {
+  return (
+    <h1
+      className="
+        text-6xl
+        font-bold
+        text-blue-500
+        hover:text-blue-50
+        hover:bg-blue-500
+        transition
+        duration-1000
+        "
+      onClick={() => alert(123)}
+    >
+      Texto no meu h1
+    </h1>
+  );
+}
